25312cf4c4af1359e8d7e2fa1edbe21cd2459d39
Kai Uwe Neumann <kai.uwe.neumann@gmail.com>
2019-05-19 12:24:34 +0200

    better docs

--- build.sh
+++ build.sh
@@ -6,23 +6,37 @@
 #     `-'`-'`----'`-'`-'       kai.uwe.neumann@gmail.com
 #
 #
-
-# Builds i3-gaps for Debian stretch from source in a docker container
-
-# Based on https://github.com/maestrogerardo/i3-gaps-deb/blob/master/i3-gaps-deb
-# Dockerfile example taken from https://github.com/IronicBadger/docker-snapraid/blob/master/Dockerfile
+# Build i3-gaps window manager for Debian stretch from source in a docker container
+#
+# Using i3-gaps fork from https://www.github.com/Airblader/i3
+# Based on installer script https://github.com/maestrogerardo/i3-gaps-deb/blob/master/i3-gaps-deb
+# Converted to dockerized build, inspired by https://github.com/IronicBadger/docker-snapraid/
+#
+# Usage:
+#
+# 1) Build on docker host
+#   git clone https://gitlab.kaiuweneumann.de/kai/build-i3-gaps.git
+#   cd build-i3-gaps
+#   chmod +x build.sh
+#   ./build.sh
+#
+# 2) install from build packages
+#   sudo dpkg -i build/i3*.deb
+#
 
 IMAGE_TAG="i3-gaps-build"
 
-#git clone https://gitlab.kaiuweneumann.de/kai/build-i3-gaps.git
-#cd build-i3-gaps
-
+#build from dockerfile
 docker build -t $IMAGE_TAG .
+
+#create container and get volume
 ID=$(docker create $IMAGE_TAG)
+
+#copy built deb packages from volume
 docker cp $ID:/build/ .
+
+#remove container including all volumes
 docker rm -v $ID
+
+#remove build image
 docker rmi $IMAGE_TAG
-
-cd build/
-
-# dpkg -i i3*.deb
